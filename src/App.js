5dfa76e6b41e8884fbafe8bccebdd1e338fa3c0b
RichardPMachado <richardmachado2211@gmail.com>
2022-08-23 19:54:38 -0300

    Requisito 3 - Completo - Criando component Card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 import './app.css';
 
 export default class App extends Component {
@@ -10,6 +11,7 @@ export default class App extends Component {
           <h1>Tryunfo</h1>
         </header>
         <Form />
+        <Card />
       </>
     );
   }
