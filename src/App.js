4666f27f6ab08223c8b4848b8b87ad91e202d3b6
RichardPMachado <richardmachado2211@gmail.com>
2022-08-24 00:14:14 -0300

    Requisito 4 - completo

--- src/App.js
+++ src/App.js
@@ -4,14 +4,85 @@ import Card from './components/Card';
 import './app.css';
 
 export default class App extends Component {
+  state = {
+    cardName: '',
+    cardDescription: '',
+    cardAttr1: '',
+    cardAttr2: '',
+    cardAttr3: '',
+    cardImage: '',
+    cardRare: '',
+    cardTrunfo: '',
+    // hasTrunfo: false,
+    isDisable: true,
+
+  };
+
+  onInputChange = ({ target }) => {
+    const { name, type } = target;
+    const value = type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    }, () => {
+      const {
+        cardName, cardDescription, cardAttr1,
+        cardAttr2, cardAttr3, cardImage, cardRare, isDisable,
+      } = this.state;
+
+      if (cardName && cardDescription
+        && cardAttr1 && cardAttr2 && cardAttr3
+        && cardImage && cardRare) {
+        this.setState({ isDisable });
+      } else {
+        this.setState({ isDisable: true });
+      }
+    });
+  };
+
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      isDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    } = this.state;
     return (
       <>
         <header>
           <h1>Tryunfo</h1>
         </header>
-        <Form />
-        <Card />
+        <main className="container">
+          <Form
+            cardName={ cardName }
+            cardDescription={ cardDescription }
+            cardAttr1={ cardAttr1 }
+            cardAttr2={ cardAttr2 }
+            cardAttr3={ cardAttr3 }
+            cardImage={ cardImage }
+            cardRare={ cardRare }
+            cardTrunfo={ cardTrunfo }
+            isDisabled={ isDisabled }
+            onInputChange={ this.onInputChange }
+            onSaveButtonClick={ this.onSaveButtonClick }
+          />
+          <Card
+            cardName={ cardName }
+            cardDescription={ cardDescription }
+            cardAttr1={ cardAttr1 }
+            cardAttr2={ cardAttr2 }
+            cardAttr3={ cardAttr3 }
+            cardImage={ cardImage }
+            cardRare={ cardRare }
+            cardTrunfo={ cardTrunfo }
+          />
+        </main>
       </>
     );
   }
